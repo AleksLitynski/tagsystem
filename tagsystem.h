6bbaf9b4bd9829529738b279706184a99b4933b7
Aleks Litynski <tavern.joe@gmail.com>
2016-03-07 00:48:21 -0500

    notes on node data structure

--- tagsystem.h
+++ tagsystem.h
@@ -11,6 +11,24 @@ typedef struct {
 typedef struct { 
     MDB_env * env; char * docs; char * index; char * iIndex; 
 } ts_env;
+typedef struct {
+    // probably need to pack these 3 values by hand
+    unsigned int pathSize: 8; // max 160, needs 8 bits
+    unsigned int maskMask: 20; // (this should often be less than 20, as the path will often be less than 20) allignment of masks relative to path (size: pathLength/bits-in-a-char)
+    unsigned int jumpSize: 8; // total number of jumps (max 160)
+
+    char * path; // 1 - 160 bits of data
+    char * mask; // series of 1 char masks
+    void * jumps; // the jumps indicated by the mask
+
+    // [ pl ][ mm ][ gc ][ path ][ jumps ]
+    //  a, b, c, d | e, f, g, h | i, j, k, l // pathLength: 12 
+    //  0, 0, 1, 0 |            | 0, 0, 0, 1 // maskMask: 101
+    //  ptr, ptr, 0, 0, 0, 0, 0, 0, 0, 0 
+    //      // on delete: remove item. if more than 10 spaces, de-allocate
+    //      // on add: add to end. If no space, compact all
+    //      //      if still no space, allocate 
+}
 
 
 // api
