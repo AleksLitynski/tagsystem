2ddb18ee2505d0f890ecbc07eb78ab241b21922e
Aleks Litynski <tavern.joe@gmail.com>
2016-03-25 20:22:45 -0400

    Remove + add are framed up ok.

--- tagsystem.c
+++ tagsystem.c
@@ -87,6 +87,8 @@ void ts_doc_untag(ts_env * env, ts_doc_id * doc, char * tag) {
     // migrate all the branches to the parent, except this node
     // (can't migrate to parent, need to... something)
     // remove this node
+    
+
 
     /*
     1111111
@@ -106,9 +108,105 @@ void ts_doc_untag(ts_env * env, ts_doc_id * doc, char * tag) {
      of the branches will still be assigned to the same parent (I think?)
      */
 
+    MDB_txn * txn;
+    MDB_dbi * dbi;
+    MDB_val * key, meta, dbOut;
+    ts_tag_node current;
+    MDB_val data[2];
+    data[0].mv_data = malloc(TS_MAX_NODE_SIZE_BYTES);
+    data[1].mv_data = malloc(TS_MAX_NODE_SIZE_BYTES);
 
+    // setup transaction
+    mdb_txn_begin(env->env, NULL, 0, &txn);
+    mdb_dbi_open(txn, tag->name, MDB_INTEGERKEY, &dbi);
+    key.mv_size = sizeof(unsigned int); 
+
+    // get metadata
+    key.mv_data = 0;
+    mdb_get(txn, dbi, meta_key, meta);
+
+    // get root node
+    key->mv_data = meta->mv_data.rootId;
+    int res = mdb_get(txn, dbi, key, dbOut);
     
-    
+    if(res == MDB_NOTFOUND) {
+        // these is no root - our item has already been removed
+        mdb_txn_commit(txn);
+        return;             
+    } else {
+
+        // there is a root. Walk the tree to find our leaf
+        current.key = key->mv_data;
+        _ts_mdb_val_to_tag_node(dbOut, TS_KEY_SIZE_BITS, current);
+
+        // the depth of the current node
+        int nodeInset = 0; 
+        int maskCount = 0;
+        unsigned int prevId = current.key;
+        int prevMaskCount = 0;
+
+        int bitIndex = 0;
+        for(bitIndex < TS_KEY_SIZE_BITS; bitIndex++) {
+            int localIndex = bitIndex - nodeInset;
+            uint8_t bitHasMask = ts_util_test_bit(current->mask, localIndex);
+             
+            if(ts_util_test_bit(node->doc_id, bitIndex) == ts_util_test_bit(current->doc_id, localIndex)) {
+                if(bitHasMask) maskCount++;
+                //  this branch has it, continue
+                continue; 
+            } else if(bitHasMask) {
+                prevId = current.key;
+                prevMaskCount = maskCount;
+
+                // the other branch has it, jump, then continue
+                key->mv_data = current->jumps[maskCount];
+                mdb_get(txn, dbi, key, dbOut);  // overwrite current node
+                current.key = key->mv_data;
+                _ts_mdb_val_to_tag_node(dbOut, TS_KEY_SIZE_BITS - bitIndex, current);
+
+                nodeInset = bitIndex;           // increase the inset
+                maskCount = 0;                  // reset the mask count
+                continue;
+            } else {
+                // we coudn't find this document in the tree
+                mdb_txn_commit(&txn);
+                return;
+            }
+        }
+
+        if(bitIndex >= TS_KEY_SIZE_BITS) {
+            // remove from parent
+            MDB_val * dbOutParent;
+            key->mv_data = prevId;
+            mdb_get(txn, dbi, key, dbOutParent); 
+            // create a mask such that the jump will be turned off
+            parentData->mv_data[prevMaskCount/8] &= ~(1<<(prevMaskCount%8));
+            mdb_put(txn, dbi, key, dbOutParent);
+
+            // re-insert all children
+            int jumpNum = 0;
+            for(int i = 0; i < TS_KEY_SIZE_BITS - node_inset; i++) {
+                if(ts_util_test_bit(current->mask, i)) {
+                                    
+                    // I'm not sure if this will overwrite the value of dbOut before
+                    //      the txn completes, of if it's ok.
+                    key->mv_data = current->jumps[jumpNum];
+                    mdb_get(txn, dbi, key, dbOut);
+                    ts_tag_move(txn, &data, env, key, dbOut);
+
+                    jumpNum++;
+                }
+            }
+
+            // remove from lmdb
+            key->mv_data = current->key;
+            mdb_delete(txn, dbi, key, dbOut);
+        
+        }
+
+        mdb_txn_commit(txn);
+        free(data[0]->mv_data);
+        free(data[1]->mv_data);
 }
 
 void ts_tag_create(ts_env * env, char * tagName, ts_tag * tag) {
@@ -122,7 +220,7 @@ void ts_tag_create(ts_env * env, char * tagName, ts_tag * tag) {
     meta_key.mv_size = sizeof(unsigned int); 
     meta_key.mv_data = 0;
     mdb_txn_begin(env->env, NULL, 0, &txn);
-    mdb_dbi_open(txn, db_name, MDB_CREATE | MDB_INTEGERKEY, &dbi);
+    mdb_dbi_open(txn, tag->name, MDB_CREATE | MDB_INTEGERKEY, &dbi);
     int res = mdb_get(txn, dbi, meta_key, meta_data);
     if(res == MDB_NOTFOUND) {
         meta_data->rootId = 1;
@@ -139,25 +237,31 @@ void ts_tag_close(ts_env * env, ts_tag * tag) {
 
 void ts_tag_insert(ts_env * env, ts_tag * tag, ts_doc_id * doc) {
     uint8_t mask[TS_KEY_SIZE_BYTES] = {0};
-
     ts_tag_node node;
+    MDB_txn * txn;
+    MDB_val data[2];
+
+    data[0].mv_data = malloc(TS_MAX_NODE_SIZE_BYTES);
+    data[1].mv_data = malloc(TS_MAX_NODE_SIZE_BYTES);
+    mdb_txn_begin(txn, &data, env->env, NULL, 0, &txn);
     node.key = 0;
     node.doc_id = doc;
     mode.mask = &mask;
     ts_tag_move(env, tag, &node);
+    mdb_txn_commit(&txn);
+    free(data[0]->mv_data);
+    free(data[1]->mv_data);
 
 }
 
-void ts_tag_move(ts_env * env, ts_tag * tag, ts_tag_node * node) {
+void ts_tag_move(MDB_txn * txn, MDB_val * new_data, ts_env * env, ts_tag * tag, ts_tag_node * node) {
     // variables
-    MDB_txn * txn;
     MDB_dbi * dbi;
     MDB_val * key, meta, dbOut;
     ts_tag_node current;
 
     // setup transaction
-    mdb_txn_begin(env->env, NULL, 0, &txn);
-    mdb_dbi_open(txn, db_name, MDB_INTEGERKEY, &dbi);
+    mdb_dbi_open(txn, tag->name, MDB_INTEGERKEY, &dbi);
     key.mv_size = sizeof(unsigned int); 
 
     // get metadata
@@ -171,17 +275,14 @@ void ts_tag_move(ts_env * env, ts_tag * tag, ts_tag_node * node) {
     if(res == MDB_NOTFOUND) {
         node->key = key->mv_data;
 
-        MDB_val newNode;
-        newNode->mv_data = malloc(TS_MAX_NODE_SIZE_BYTES);
         _ts_tag_node_to_mdb_val(
             node, 
             TS_KEY_SIZE_BITS, 0, 
             0, 0, 
-            &newNode);
+            &new_data[0]);
 
-        mdb_put(txn, dbi, key, newNode);
-        mdb_txn_commit(txn);
-        free(newNode->mv_data);
+        mdb_put(txn, dbi, key, new_data[0]);
+        free(new_data[0]->mv_data);
         return;             
  
     } else {
@@ -216,47 +317,36 @@ void ts_tag_move(ts_env * env, ts_tag * tag, ts_tag_node * node) {
                 //  neither branch exists. Insert the data
 
                 // add the new node
-                MDB_val newNode;
-                newNode->mv_data = malloc(TS_MAX_NODE_SIZE_BYTES);
                 // copy the mask/jumps the user provided
                 _ts_tag_node_to_mdb_val(
                     node, 
                     TS_KEY_SIZE_BITS - bitIndex, bitIndex, 
                     0, 0, 
-                    &newNode);
+                    &new_data[0]);
                 if(node->key == 0) {
                     // if the user provided a key, use it
                     node->key = meta->mv_data.nextId;
                     meta->mv_data.nextId++;
                 }
                 key->mv_data = node->key;
-                mdb_put(txn, dbi, key, &newNode);
+                mdb_put(txn, dbi, key, &new_data[0]);
 
                 // update parent mask
                 int parentTotalSize = current->mv_size + sizeof(unsigned int);
                 int parentDataSize = TS_KEY_SIZE_BITS - nodeInset;
 
-                MDB_val parentNode;
-                parentNode->mv_data = malloc(TS_MAX_MODE_SIZE_BYTES);
                 _ts_tag_node_to_mdb_val(
                     current, 
                     TS_KEY_SIZE_BITS - nodeInset, nodeInset, 
                     newNode->key, bitIndex - nodeInset, 
-                    &parentNode);
+                    &new_data[1]);
 
                 key->mv_data = current->key;
-                mdb_put(txn, dbi, key, parentNode);
-
-                mdb_txn_commit(txn);
-                free(newNode->mv_data);
-                free(parentNode->mv_data);
+                mdb_put(txn, dbi, key, new_data[1]);
                 return;             
             }
         }
     }
-
-    //if we make it to here, the item was already in the tree
-    mdb_txn_commit(txn);
 }
 
 
