8ee0ca5cebc1992d1c249f01cfd4d5976bd20896
Aleks Litynski <tavern.joe@gmail.com>
2016-04-19 08:23:34 -0400

    iter stopped seg-faulting. Now it just has a bug

--- utils/iter.c
+++ utils/iter.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-
+#include <stdio.h> 
 #define args(...) __VA_ARGS__
-#define iter(TYPE, SETUP, ITEM, BODY) \
-    TYPE ## _set * ITEM ## _set; \
-    TYPE ## _item * ITEM; \
-    TYPE ## _init(SETUP, ITEM ## _set, ITEM); \
-    while(TYPE ## _next(ITEM ## _set, ITEM)) BODY; \
-    TYPE ## _close(ITEM ## _set, ITEM); 
+#define iter(TYPE, SETUP, ITEM, BODY) { \
+    TYPE ## _set ITEM ## _set; \
+    TYPE ## _item ITEM; \
+    TYPE ## _init(SETUP, &ITEM ## _set, &ITEM); \
+    while(TYPE ## _next(&ITEM ## _set, &ITEM)) BODY; \
+    TYPE ## _close(&ITEM ## _set, &ITEM); \
+    }
 
 
 typedef struct {
@@ -15,21 +15,25 @@ typedef struct {
 } test_iter_set;
 typedef int test_iter_item;
 
-int test_iter_init(int low, int high, test_iter_set * set, test_iter_item * item) {
-    printf("%i\n", low);
-    printf("%i\n", high);
-    *item = 0;
-    set->to = high;
+int test_iter_init(int low, int high, 
+    test_iter_set * set, test_iter_item * item) {
     set->current = low;
+    set->to = high;
+    *item = (test_iter_item) 0;
     return 0;
 }
 
 int test_iter_next(test_iter_set * set, test_iter_item * item) {
+
+    printf("test_iter_next\n");
+    printf("%i\n", set->current);
     if(set->current > set->to) {
+            printf("first\n");
         *item = set->current;
         set->current++;
         return 0;
     }
+            printf("second\n");
     return 1;
 }
 
@@ -37,6 +41,9 @@ int test_iter_close(test_iter_set * set, test_iter_item *item) {
     return 0;
 }
 
+void mk(test_iter_set ** j) {
+    *j = malloc(sizeof(test_iter_set));
+}
 
 int main(int argc, char * argv[]) {
 
